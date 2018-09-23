2a055826fc35a0288e95b3aec6bf1925c9d24eb4
ThetaHacker <42395099+ThetaHacker@users.noreply.github.com>
2018-09-23 13:26:51 +1000

    Update code.js

--- code.js
+++ code.js
@@ -3,12 +3,9 @@ new (function() {
 	var descriptor = {
 		blocks: [
 			[' ','Display banner %s','gbanner'],
-      [' ','Display alert %s','gshout'],
-      [' ','Console log %s','glog'],
-      ['h','Start automatically','gauto'],
-      ['r','Pi','getpi',false],
-      ['r','E','gete',false],
-      ['b','%s != %s','getnotequal',false,false]
+      			[' ','Display alert %s','gshout'],
+      			[' ','Console log %s','glog'],
+      			['h','Start automatically','gauto'],
 		],
 		url : '' // This is the URL for when someone clicks "learn more"
 	};
@@ -19,28 +16,60 @@ new (function() {
 	ext._getStatus = function() {
 		return {status:2, msg:'Ready'}; // Returning status defines the colour of the light beside the blocks.
 	};
-  ext.gbanner = function(Banner) {    
-    ScratchExtensions.notify(Banner);
-  };
-  ext.gshout = function(Shout) {    
-    alert(Shout);
-  };
-  ext.glog = function(Log) {    
-    console.log(Log)
-  };
-  ext.gauto = function() {    
-    return true;
-  };
-  ext.getpi = function() {
-    return 3.141592653589793238462643389;
-  };
-  ext.gete = function() {
-    return 2.718281828459045235360287471;
-  };
-   ext.getnotequal = function(n1,n2) {
-    return n1 != n2;
-  };
+	
+	// Scratch++ Functions
+  	ext.gbanner = function(Banner) {    
+    		ScratchExtensions.notify(Banner);
+  	};
+  	ext.gshout = function(Shout) {    
+    		alert(Shout);
+  	};
+  	ext.glog = function(Log) {    
+    		console.log(Log)
+  	};
+  	ext.gauto = function() {    
+    		return true;
+  	};
 
 	ScratchExtensions.register('Scratch++', descriptor, ext); // Name of the extension.
-  ScratchExtensions.notify('Scratch++ Successfully installed');
+  	
+	console.log("Scratch++ Successfully installed")
 })();
+new (function() {
+	var ext = this;
+	var descriptor = {
+		blocks: [
+      			['r','%s ^ %s','getpow',false],
+      			['r','Pi','getpi',false],
+      			['r','E','gete',false],
+      			['b','%s != %s','getnotequal',false,false]
+		],
+		url : '' // This is the URL for when someone clicks "learn more"
+	};
+	ext._shutdown = function() {
+		
+	};
+	
+	ext._getStatus = function() {
+		return {status:2, msg:'Ready'}; // Returning status defines the colour of the light beside the blocks.
+	};
+	
+	// Scratch++ (Math/Logic) Functions
+  	ext.getpi = function() {
+    		return 3.141592653589793238462643389;
+  	};
+  	ext.gete = function() {
+    		return 2.718281828459045235360287471;
+  	};
+   	ext.getnotequal = function(n1,n2) {
+    		return n1 != n2;
+  	};
+   	ext.getpow = function(n1,n2) {
+    		return Math.pow(n1,n2);
+  	};
+
+	ScratchExtensions.register('Scratch++ (Math/Logic)', descriptor, ext); // Name of the extension.
+	console.log("Scratch++ (Math/Logic) Successfully installed")
+})();
+
+ScratchExtensions.notify('Scratch++ Successfully installed');
