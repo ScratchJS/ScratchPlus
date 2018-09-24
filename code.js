48e7f02145f67d17ca49931009db05acb7aa8f7a
ThetaHacker <42395099+ThetaHacker@users.noreply.github.com>
2018-09-24 12:58:09 +1000

    Update code.js

--- code.js
+++ code.js
@@ -39,7 +39,7 @@ new (function() {
 	var ext = this;
 	var descriptor = {
 		blocks: [
-      			['r','%n ^ %n','getpow',false],
+      			['r','%n ^ %n','getpow',10,10,false],
       			['r','Pi','getpi',false],
       			['r','E','gete',false],
       			['b','%s != %s','getnotequal',false,false]
